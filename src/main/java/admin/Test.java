f1c13eba3af76e759ee64b6580e7a9e08be58c4a
gfquuuuu <13714947594@163.com>
2018-03-15 21:13:56 +0800

    2号第一次提交

--- src/main/java/admin/Test.java
+++ src/main/java/admin/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		TestSingleton.getInstance().printStr("恶汉式单例模式测试类");
-		TestSingleton2.getInstance().printStr("恶汉是单例模式测试类");
+		TestSingleton2.getInstance().printStr("恶汉是单例模式测");
 		StaticSingleton.getInstancee().printStr("内部类单例模式");
 	}
 
