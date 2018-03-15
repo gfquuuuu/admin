2e6a5c10b76c351dbf01371232de7bcab7b08dd0
gfquuuuu <13714947594@163.com>
2018-03-15 21:40:40 +0800

    2号第三次提交

--- src/main/java/admin/Test.java
+++ src/main/java/admin/Test.java
@@ -6,7 +6,7 @@ public class Test {
 		TestSingleton.getInstance().printStr("恶汉式单例模式测试类");
 		TestSingleton2.getInstance().printStr("恶汉是单例模式测");
 		StaticSingleton.getInstancee().printStr("内部类单例模式");
-		String tt = "node2";
+		String tt = "node22";
 	}
 
 }
